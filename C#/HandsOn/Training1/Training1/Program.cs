6525c17b74edf924114a613de60806835557ec8d
Bhargav Vasupalli <bhargavv@infinite.com>
2024-10-28 14:57:32 +0530

    infinite Training

---
+++ C#/HandsOn/Training1/Training1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace Training1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* --------------------Basics-------------------*/
+
+            Console.WriteLine("enter name");
+            String name = Console.ReadLine();
+            Console.WriteLine("Enter number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(name+" "+num);
+
+
+            /* -----------------Data Conversion and Type Casting---------------*/
+            int i = 10;
+            float f = i;
+            Console.WriteLine(f);
+            int t = (int)f;
+            Console.WriteLine(t);
+            Console.ReadLine();
+
+            /* ----------------Parse and TryParse-----------------*/
+            String str = "31";
+            int n = int.Parse(str);
+            Console.WriteLine(n);
+
+
+        }
+    }
+}
