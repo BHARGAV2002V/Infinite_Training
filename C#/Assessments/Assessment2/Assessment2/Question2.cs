d61dacd08718aad5babf8ac6c5a11d7d9c219952
Bhargav Vasupalli <bhargavv@infinite.com>
2024-11-13 11:05:59 +0530

    Assessment 2

---
+++ C#/Assessments/Assessment2/Assessment2/Question2.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment2
+{
+    /*    2. Create a Class called Products with Productid, 
+            Product Name, Price. Accept 10 Products, sort them based on the price, and display the sorted Products*/
+    class Question2
+    {
+        static void Main(string[] args)
+        {
+            List<Products> p_list = new List<Products>();
+            Console.WriteLine("Enter Details of products");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Enter product {i + 1}");
+                Console.WriteLine("Enter product Id");
+                int prodid = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter product Name");
+                string prodname = Console.ReadLine();
+                Console.WriteLine("Enter product price");
+                float price = Convert.ToSingle(Console.ReadLine());
+                p_list.Add(new Products(prodid, prodname, price));
+
+            }
+
+
+
+
+            p_list.Sort();
+
+            Console.WriteLine("---------After sorting by price----------");
+            foreach (Products p in p_list)
+            {
+                Console.WriteLine(p.ToString());
+            }
+            Console.ReadLine();
+        }
+        public class Products : IComparable<Products>
+        {
+            public int prodid;
+            public string prodname;
+            public float price;
+
+            public Products(int prodid, string prodname, float price)
+            {
+                this.prodid = prodid;
+                this.prodname = prodname;
+                this.price = price;
+            }
+
+            public override string ToString()
+            {
+                return $"Product Name : {prodname}, product Id : {prodid} , product price : {price}";
+            }
+
+            public int CompareTo(Products p)
+            {
+                return this.price.CompareTo(p.price);
+            }
+
+        }
+    }
+}
+
+  
+    
+
+  
+
---
+++ C#/Assessments/Assessment2/Assessment2/Question3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment2
+{
+/*    3. Write a C# program to implement a method that takes an integer as 
+        input and throws an exception if the number is negative. Handle the exception in the calling code.*/
+   class Negative_exception : ApplicationException
+    {
+        public Negative_exception(string message) : base(message) { }
+    }
+    class Question3
+    {
+        public static void checknumber(int n)
+        {
+            if (n < 0)
+            {
+                throw (new Negative_exception("Exception : Number is negative"));
+            }
+            else
+            {
+                Console.WriteLine("Number is Positive");
+            }
+        }
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Enter a number");
+                int n = int.Parse(Console.ReadLine());
+                checknumber(n);
+            }
+            catch(Negative_exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
