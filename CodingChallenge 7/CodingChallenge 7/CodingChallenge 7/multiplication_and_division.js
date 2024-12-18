56b0693f4ec21dff8f9bae2bd8e95e382958bbca
Bhargav Vasupalli <bhargavv@infinite.com>
2024-12-18 11:38:27 +0530

    CC7

---
+++ CodingChallenge 7/CodingChallenge 7/CodingChallenge 7/multiplication_and_division.js
@@ -0,0 +1,11 @@
+﻿function multiply() {
+    num1 = document.getElementById("firstnum").value;
+    num2 = document.getElementById("secondnum").value;
+    document.getElementById("res").innerHTML = num1 * num2;
+}
+
+function division() {
+    num1 = document.getElementById("firstnum").value;
+    num2 = document.getElementById("secondnum").value;
+    document.getElementById("res").innerHTML = num1 / num2;
+}
